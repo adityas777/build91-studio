2684cb7a32a16d16e7324f8afe4ce88207c1f7d0
Your Name <your-email@example.com>
2026-08-17 14:08:08 +0530

    fix: set images.unoptimized to true to resolve S3 loading issues

--- next.config.js
+++ next.config.js
@@ -2,12 +2,13 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
+    unoptimized: true,
     remotePatterns: [
       { protocol: 'https', hostname: 'i.ytimg.com' },
       { protocol: 'https', hostname: 'img.youtube.com' },
       { protocol: 'https', hostname: 'i.vimeocdn.com' },
-      { protocol: 'https', hostname: '*.s3.amazonaws.com' },
-      { protocol: 'https', hostname: '*.s3.*.amazonaws.com' },
+      { protocol: 'https', hostname: '**.amazonaws.com' },
+      { protocol: 'https', hostname: 'build91-studio-prod-portfoliobucketbucket-bsmmcecr.s3.us-east-1.amazonaws.com' },
     ],
   },
 };
